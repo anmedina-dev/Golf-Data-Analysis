945af05225776269aeecd914a9efe1e0ba794a18
Antranik Medina <anmedina@syr.edu>
2020-08-20 01:52:52 -0400

    progress, 1:52AM rn. Need to sleep

--- webscrape.py
+++ webscrape.py
@@ -1,45 +1,52 @@
+#libraries
 from urllib.request import urlopen
-
 from urllib.error import HTTPError
-
 import requests
-
 from bs4 import BeautifulSoup
 import csv
 
+#data: year, event, event scoring average, rankTW, rankLW, name, rounds, avg, totalStrokes, totalAdjustment, totalRounds
 stats = []
 
-
 try:
-
-    html = urlopen("https://www.pgatour.com/content/pgatour/stats/stat.120.y2020.eoff.t013.html")
-    url = "https://www.pgatour.com/content/pgatour/stats/stat.120.y2020.eoff.t013.html"
-    r = requests.get(url)
-
+    html = urlopen("https://www.pgatour.com/stats/stat.120.y2020.eoff.t013.html")
 except HTTPError as e:
-
+    #print HTTPError
     print(e)
-
 else:
 
-    res = BeautifulSoup(html.read(),"html5lib")
-    start = 1
+    soup = BeautifulSoup(html.read(),"html5lib")
 
-    table = res.find('tbody')
+    tournamentSeason = soup.find('select', attrs = {'class': 'statistics-details-select--season'})
+    #print(table)
+    #print(tournament)
 
+    #get data from every tournament from every year
+    #every year loop
+    for szn in tournamentSeason.findAll('option'):
+        golfX = {}
+        golfX['year'] =
+        tournamentEvent = soup.find('select', attrs = {'class': 'statistics-details-select--tournament'})
+        table = soup.find('table', attrs = {'id': 'statsTable'}).tbody
+        for tmnt in tournamentEvent.findAll('option'):
+            golfX['urlValue'] = tmnt['value']
+            #print(event['urlValue'])
+            golfX['title'] = tmnt.text
+"""
     for row in table.findAll('tr'):
         player = {}
         player['rankTW'] = row.td.text
+        player['rankLW'] = row.td.text
         print(player['rankTW'])
+"""
+    #cats = soup.findAll("span", {"class": ["customSelectInner"]} )
+    #tags = soup.findAll("tr")
 
-    #cats = res.findAll("span", {"class": ["customSelectInner"]} )
-    #tags = res.findAll("tr")
-    
 
     #for cat in cats:
     #   print(cat.getText())
-    
+
     #for tag in tags:
      #   print(tag.getText())
 
-    #print(res.title)
+    #print(soup.title)
