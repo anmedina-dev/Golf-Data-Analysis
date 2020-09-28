c95f286c2585578053a386ec77fb3cb52e22672f
Antranik Medina <anmedina@syr.edu>
2020-09-28 02:53:21 -0400

    WORKING SCORING SCRAPE

--- functions.py
+++ functions.py
@@ -5,8 +5,13 @@ import requests
 from bs4 import BeautifulSoup
 
 #changes the url to a different event in the same year
-def eventURLChanger(eventURL):
-    html = urlopen("https://www.pgatour.com/content/pgatour/stats/stat.120.y2020.eoff.{}.html".format(eventURL))
+def eventURLChanger(year, eventURL):
+    html = urlopen("https://www.pgatour.com/content/pgatour/stats/stat.120.{}.eoff.{}.html".format(year, eventURL))
+    soup = BeautifulSoup(html.read(),"html5lib")
+    return(soup)
+
+def eventNoURLChanger(year):
+    html = urlopen("https://www.pgatour.com/content/pgatour/stats/stat.120.{}.html".format(year))
     soup = BeautifulSoup(html.read(),"html5lib")
     return(soup)
 
--- webscrape.py
+++ webscrape.py
@@ -5,50 +5,56 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import functions
+import time
 
 #data: year, event, event scoring average, rankTW, rankLW, name, rounds, avg, totalStrokes, totalAdjustment, totalRounds
 stats = []
 
-#t060 is the most recent tournament
-soup = functions.eventURLChanger('t060')
+#t060 is the most recent and the last tournament of the year
+soup = functions.eventURLChanger('y2020', 't060')
 tournamentSeason = soup.find('select', attrs = {'class': 'statistics-details-select--season'})
 
+#Timing how long it takes to scrape
+start = time.time()
 
 #get data from every tournament from every year
 #every year loop
-"""
 for szn in tournamentSeason.findAll('option'):
     golfX = {}
     golfX['year'] = szn.text
     golfX['yearURL'] = szn['value']
+    soupY = functions.eventNoURLChanger(golfX['yearURL'])
+    tournamentEvent = soupY.find('select', attrs = {'class': 'statistics-details-select--tournament'})
+    for tmnt in tournamentEvent.findAll('option'):
+        #golfX = {}
+        golfX['eventURL'] = tmnt['value']
+        golfX['title'] = tmnt.text
+        #print(golfX['title'])
+        print(golfX['year'] + ' ' + golfX['title'])
+        soupT = functions.eventURLChanger(golfX['yearURL'], golfX['eventURL'])
+        table = soupT.find('table', attrs = {'id': 'statsTable'}).tbody
+        for row in table.findAll('tr'):
+            golfX['rankTW'] = row.select('td')[0].text.strip()
+            golfX['rankLW'] = row.select('td')[1].text.strip()
+            golfX['name'] = row.select('td')[2].text.strip()
+            golfX['rounds'] = row.select('td')[3].text
+            golfX['avg'] = row.select('td')[4].text
+            golfX['totalStrokes'] = row.select('td')[5].text
+            golfX['totalAdjustment'] = row.select('td')[6].text
+            golfX['totalRounds'] = row.select('td')[7].text
+            #functions.printGolfX(golfX)
+            stats.append([golfX['year'], golfX['title'], golfX['rankTW'], golfX['rankLW'], golfX['name'], golfX['rounds'], golfX['avg'], golfX['totalStrokes'], golfX['totalAdjustment'], golfX['totalRounds']])
+            #stats.append([golfX['eventURL'], golfX['title'], golfX['rankTW'], golfX['rankLW'], golfX['name'], golfX['rounds'], golfX['avg'], golfX['totalStrokes'], golfX['totalAdjustment'], golfX['totalRounds']])
 
-"""
-tournamentEvent = soup.find('select', attrs = {'class': 'statistics-details-select--tournament'})
-table = soup.find('table', attrs = {'id': 'statsTable'}).tbody
-for tmnt in tournamentEvent.findAll('option'):
-    golfX = {}
-    golfX['eventURL'] = tmnt['value']
-    #print(event['urlValue'])
-    golfX['title'] = tmnt.text
-    #golfX['event']
-    print(golfX['title'])
-    table = soup.find('table', attrs = {'id': 'statsTable'}).tbody
-    for row in table.findAll('tr'):
-        golfX['rankTW'] = row.select('td')[0].text.strip()
-        golfX['rankLW'] = row.select('td')[1].text.strip()
-        golfX['name'] = row.select('td')[2].text.strip()
-        golfX['rounds'] = row.select('td')[3].text
-        golfX['avg'] = row.select('td')[4].text
-        golfX['totalStrokes'] = row.select('td')[5].text
-        golfX['totalAdjustment'] = row.select('td')[6].text
-        golfX['totalRounds'] = row.select('td')[7].text
-        #functions.printGolfX(golfX)
-        stats.append([golfX['eventURL'], golfX['title'], golfX['rankTW'], golfX['rankLW'], golfX['name'], golfX['rounds'], golfX['avg'], golfX['totalStrokes'], golfX['totalAdjustment'], golfX['totalRounds']])
 
+end = time.time()
+print()
+print((end-start))
 
 
 filename = 'golf_scoring_stats.csv'
-fields = ['eventURL','title','rankTW','rankLW','name', 'rounds', 'avg', 'totalStrokes', 'totalAdjustment', 'totalRounds']
+#fields = ['eventURL','title','rankTW','rankLW','name', 'rounds', 'avg', 'totalStrokes', 'totalAdjustment', 'totalRounds']
+fields = ['year', 'title','rankTW','rankLW','name', 'rounds', 'avg', 'totalStrokes', 'totalAdjustment', 'totalRounds']
 with open(filename, 'w') as f:
     w = csv.writer(f)
     w.writerow(fields)
