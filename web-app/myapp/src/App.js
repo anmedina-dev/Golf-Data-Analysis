0a6ff0e2ea96020441a1d3ab177cf0ffebd67dab
Antranik Medina <anmedina@syr.edu>
2020-10-19 20:08:29 -0400

    cleaned up App.js and made some progress

--- web-app/myapp/src/App.js
+++ web-app/myapp/src/App.js
@@ -4,99 +4,131 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css'
 import axios from 'axios'
+var _ = require('underscore')
 
 function App() {
 
-  const [years, setYears] = useState([]);
-  const [tourneys, setTourneysDropdown] = useState([]);
+  //Working on right now
+  const [distinctPlayerYears, setDistinctYears] = useState([]);
+
+  //Player 
+  const [stats, setStats] = useState([]);
+  //Dropdown menu options 
+  const [years, setYearsDropdown] = useState([]);
+  const [tourneys, setTourneysDropdown] = useState([]); 
+  const [playerTourns, setPlayerDropdown] = useState([]);
+  //temp for the dataoutput
   const [scores, setScores] = useState([]);
-  const [playerTourns, setPlayerTourns] = useState([]);
+  //Option menu selected
   const [sYear, setSelectYear]  = useState(0);
   const [sTourney, setSelectTourney]  = useState('');
   const [sPlayer, setSelectPlayer] = useState('');
 
+  ////////    useEffects 
   useEffect(() => {
     getTournScores();
   }, []);
 
   useEffect(() => {
     getThemYears();
-  }, []);
+  }, [years]);
 
   useEffect(() =>{
     getTournDropdown();
   }, [tourneys]);
 
+  useEffect(() =>{
+    getPlayerDropdown();
+  }, [playerTourns])
+
+  useEffect(() => {
+    getPlayerStats();
+  }, [stats])
+
+  useEffect(() => {
+    getDistinctPlayedYears();
+  })
 
 
 
-  //Get specific tournament
-  const getTournScores = async(e=0) => {
-    if(e){
-      e.preventDefault();    
-      const {tournamentTitle, year} = e.target
-      let temp = `/api/golf/${year.value}/${tournamentTitle.value}`
+
+
+  //////////DROPDOWN MENU GETTERS//////////  
+  //Get players in a specific tournament in a specific year
+  //Fix to update (not necessary)
+  const getTournScores = async(option=0) => {
+    if(option){
+
+      let temp = `/api/golf/everytourney/${sYear.value}/${option.label}`
       const res = await axios.get(temp)
       const data = res.data
       setScores(data)
-      year.value = ''
-      tournamentTitle.value = ''
     }else{
       const res = await axios.get('/api/golf')
       const data = res.data
       setScores(data)
     }
   }
-
+  //Get years for dropdown menu
   const getThemYears = async() => {
     const res = await axios.get('/api/golf/getyears')
-    setYears(res.data)
-    //console.log(years)
+    setYearsDropdown(res.data)
   }
 
+  //Get tournaments for the dropdown menu
   const getTournDropdown = async() => {
-    let temp = `/api/golf/${sYear.value}`
+    let temp = `/api/golf/specificyear/${sYear.value}`
     const res = await axios.get(temp)
     const data = res.data
     setTourneysDropdown(data)
   }
 
-  function newYearSetter(option){   
-    setSelectYear(option)
-    console.log(option.label)
-    //console.log(years)
-    //console.log(sYear)
-    getTournDropdown()
-    console.log(scores)
-  }
-
-  const newTourneySetter = async(option) =>{   
-    setSelectTourney(option)
-    console.log(sYear)
-    console.log(option.label)
-
-    let temp = `/api/golf/${sYear.value}/${option.label}`
-    let res = await axios.get(temp)
-    let data = res.data
-    setScores(data)
-    temp = `/api/golf/player/${sYear.value}/${option.label}`
-    res = await axios.get(temp)
-    data = res.data
-    setPlayerTourns(data)
-  }
-
-  const newPlayerDropSetter = async(option) =>{   
-    setSelectPlayer(option)
-    console.log(sPlayer)
-    console.log(option.label)
-
-    let temp = `/api/golf/${option.label}`
+  //Get names for the dropdown menu
+  const getPlayerDropdown = async() => {
+    const temp = `/api/golf/player/${sYear.value}/${sTourney.value}`
     const res = await axios.get(temp)
     const data = res.data
-    setPlayerTourns(data)
+    setPlayerDropdown(data)
+  }
+
+  //Get all stats of a players from all tournaments found in database
+  const getPlayerStats = async() => {
+    //console.log(sPlayer.value)
+    let temp = `/api/golf/stat/${sPlayer.value}`
+    const res = await axios.get(temp)
+    const data = res.data
+    //console.log(data)
+    setStats(data)
+  }
+
+  const getDistinctPlayedYears = () => {
+    findYearsInPlayerCareer()
+  }
+
+  //// OPTIONS MENU ONSUBMIT FUNCTIONS///////
+  //When a year is selected
+  function newYearSetter(option){   
+    setSelectYear(option)
+  }
+
+  //When a tournament is selected
+  const newTourneySetter = async(option) =>{   
+    setSelectTourney(option)
+  }
+
+  //When a player is selected
+  const newPlayerDropSetter = async(option) =>{   
+    setSelectPlayer(option)
   }
 
 
+  //////Misc. functions
+
+  //Find the years a player has played
+  const findYearsInPlayerCareer = () =>{
+    const dYears = _.keys(_.countBy(stats, function(stats) { return stats.Year; }));
+    //console.log(dYears)
+  }
 
   return (
     <div className="App">
@@ -105,15 +137,7 @@ function App() {
         <Dropdown classname = "myYearSelect" options={years} value={sYear} onChange={newYearSetter} placeholder="Select a year" />
         <Dropdown options={tourneys} value={sTourney} onChange={newTourneySetter} placeholder="Select a tournament" />
         <Dropdown options={playerTourns} value={sPlayer} onChange={newPlayerDropSetter} placeholder="Select a player" />  
-        {/*
-        <form onSubmit={e => getTournScores(e)}>
-            <label htmlFor="tournamentTitle">Tournament:</label>
-            <input type="text" name="tournamentTitle" />
-            <label htmlFor="year">Year:</label>
-            <input type="number" name="year"/>
-            <button type="submit">Check Tournament</button>
-        </form>
-        */}
+
       </div>
       <div className="DataOutput">
         {scores.length > 0 && 
--- web-app/server/routes/api/golf.js
+++ web-app/server/routes/api/golf.js
@@ -5,6 +5,7 @@ const Golf = require('../../models/Golf')
 router.get('/test', function (req, res) {res.json({msg: 'backend works'})})
 
 // @route GET /api/golf
+
 // @desc Get golfs (public)
 router.get('/', function(req, res) {
   Golf.find({Year: {$gte: 2021}, "Title": "Corales Puntacana Resort & Club Championship"})
@@ -12,7 +13,16 @@ router.get('/', function(req, res) {
     .catch(err => res.status(404).json({msg: 'no scores found'}))
 })
 
-router.get('/:year/:tournament', function(req, res) {
+//Get all player's performances in database
+router.get('/stat/:name', function(req, res) {
+  let person = decodeURI(req.params.name)
+  Golf.find({"Name": person})
+    .then(info => res.status(200).json(info))
+    .catch(err => res.status(404).json({msg: 'no person found'}))
+})
+
+//Get data of each player in specific tournament
+router.get('/everytourney/:year/:tournament', function(req, res) {
     let tourny = decodeURI(req.params.tournament)
     let yearly = parseInt(req.params.year)
     Golf.find({Year: yearly, "Title": tourny})
@@ -20,19 +30,22 @@ router.get('/:year/:tournament', function(req, res) {
       .catch(err => res.status(404).json({msg: 'no scores found'}))
 })
 
+//Get years
 router.get('/getyears', function(req, res) {
   Golf.find().distinct("Year")
     .then(info => res.status(200).json(info))
     .catch(err => res.status(404).json({msg: 'no years found'}))
 })
 
-router.get('/:year', function(req, res) {
+//Get tournaments in a year
+router.get('/specificyear/:year', function(req, res) {
   let yearly = parseInt(req.params.year)
   Golf.find({Year: yearly}).distinct("Title")
     .then(info => res.status(200).json(info))
     .catch(err => res.status(404).json({msg: 'no years found'}))
 })
 
+//Get player names in a specific tournament
 router.get('/player/:year/:tournament', function(req, res) {
   let tourny = decodeURI(req.params.tournament)
   let yearly = parseInt(req.params.year)
@@ -41,11 +54,6 @@ router.get('/player/:year/:tournament', function(req, res) {
     .catch(err => res.status(404).json({msg: 'no scores found'}))
 })
 
-router.get('/:player', function(req, res) {
-  let playerly = decodeURI(req.params.player)
-  Golf.find({"Name": playerly})
-  .then(info => res.status(200).json(info))
-  .catch(err => res.status(404).json({msg: 'no player found'}))
-})
+
 
 module.exports = router
