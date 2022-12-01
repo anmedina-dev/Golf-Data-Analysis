5d9ea7cb5c67296485953a42ba1f2f775debba6e
anmedina-dev <antranikmedina@gmail.com>
2022-11-30 20:58:24 -0500

    update api

--- web-app/myapp/src/App.js
+++ web-app/myapp/src/App.js
@@ -1,14 +1,13 @@
-import React, {useEffect, useState} from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css'
-import axios from 'axios'
-import {Line} from 'react-chartjs-2'
-import PropagateLoader from 'react-spinners/PropagateLoader';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
+import PropagateLoader from "react-spinners/PropagateLoader";
 
 function App() {
-
   //Player statistics in each tournament
   const [stats, setStats] = useState([]);
   const [rankTWAvg, setRankTWAvg] = useState([]);
@@ -18,24 +17,25 @@ function App() {
   const [distinctPlayerYears, setDistinctYears] = useState([]);
 
   //A players played in tournaments in a year
-  const [playerPlayedTourneysInYear, setPlayerPlayedTourneysInYear] = useState([]);
+  const [playerPlayedTourneysInYear, setPlayerPlayedTourneysInYear] = useState(
+    []
+  );
 
-  //Dropdown menu options 
+  //Dropdown menu options
   const [years, setYearsDropdown] = useState([]);
-  const [tourneys, setTourneysDropdown] = useState([]); 
+  const [tourneys, setTourneysDropdown] = useState([]);
   const [playerTourns, setPlayerDropdown] = useState([]);
   //temp for the dataoutput
   const [scores, setScores] = useState([]);
   //Option menu selected
-  const [sYear, setSelectYear]  = useState(0);
-  const [sTourney, setSelectTourney]  = useState('');
-  const [sPlayer, setSelectPlayer] = useState('');
-	
-	//Loading Bar
-	const [isLoading, setIsLoading] = useState(false);
+  const [sYear, setSelectYear] = useState(0);
+  const [sTourney, setSelectTourney] = useState("");
+  const [sPlayer, setSelectPlayer] = useState("");
 
+  //Loading Bar
+  const [isLoading, setIsLoading] = useState(false);
 
-  ////////useEffects 
+  ////////useEffects
   useEffect(() => {
     getTournScores();
   }, []);
@@ -44,27 +44,26 @@ function App() {
     getThemYears();
   }, []);
 
-  useEffect(() =>{
+  useEffect(() => {
     getTournDropdown();
-		if(sTourney && sPlayer){
-			getPlayerPlayedTourneysInYear();
-		};
+    if (sTourney && sPlayer) {
+      getPlayerPlayedTourneysInYear();
+    }
   }, [sYear]);
 
-  useEffect(() =>{
+  useEffect(() => {
     getPlayerDropdown();
-  }, [sTourney])
+  }, [sTourney]);
 
   useEffect(() => {
     getPlayerStats();
-  }, [sPlayer])
-
+  }, [sPlayer]);
 
   useEffect(() => {
     getDistinctPlayedYears();
-  }, [sPlayer])
+  }, [sPlayer]);
 
-/*
+  /*
   useEffect(() => {
     getArrayTester();
   }, [rankTWAvg])
@@ -72,144 +71,162 @@ function App() {
 
   useEffect(() => {
     getRankTWAvg();
-  }, [distinctPlayerYears])
+  }, [distinctPlayerYears]);
 
   useEffect(() => {
     getPlayerPlayedTourneysInYear();
-  }, [sPlayer])
+  }, [sPlayer]);
 
   useEffect(() => {
     getPlayerScoresInYear();
-  }, [sPlayer])
+  }, [sPlayer]);
 
   useEffect(() => {
     getPlayerRanksInYear();
-  }, [sPlayer])
+  }, [sPlayer]);
 
-
-  //////////DROPDOWN MENU GETTERS//////////  
+  //////////DROPDOWN MENU GETTERS//////////
   //Get players in a specific tournament in a specific year
-  const getTournScores = async() => {
-    if(sYear && sTourney){
-      const res = await axios.get(`https://golfda.herokuapp.com/api/golf/everytourney/${sYear.value}/${sTourney.value}`)
-      const data = res.data
-      setScores(data)
-    }else{
-      const res = await axios.get('https://golfda.herokuapp.com/api/golf')
-      const data = res.data
-      setScores(data)
+  const getTournScores = async () => {
+    if (sYear && sTourney) {
+      const res = await axios.get(
+        `https://golf-da-api.onrender.com/api/golf/everytourney/${sYear.value}/${sTourney.value}`
+      );
+      const data = res.data;
+      setScores(data);
+    } else {
+      const res = await axios.get("https://golf-da-api.onrender.com/api/golf");
+      const data = res.data;
+      setScores(data);
     }
-  }
+  };
   //Get years for dropdown menu
-  const getThemYears = async() => {
-    const res = await axios.get('https://golfda.herokuapp.com/api/golf/getyears')
-    const data = res.data
-    setYearsDropdown(data.reverse())
-  }
+  const getThemYears = async () => {
+    const res = await axios.get(
+      "https://golf-da-api.onrender.com/api/golf/getyears"
+    );
+    const data = res.data;
+    setYearsDropdown(data.reverse());
+  };
 
   //Get tournaments for the dropdown menu
-  const getTournDropdown = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/specificyear/${sYear.value}`)
-    const data = res.data
-    setTourneysDropdown(data)
-  }
+  const getTournDropdown = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/specificyear/${sYear.value}`
+    );
+    const data = res.data;
+    setTourneysDropdown(data);
+  };
 
   //Get names for the dropdown menu
-  const getPlayerDropdown = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/player/${sYear.value}/${sTourney.value}`)
-    const data = res.data
+  const getPlayerDropdown = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/player/${sYear.value}/${sTourney.value}`
+    );
+    const data = res.data;
     //console.log(sTourney.value)
     //console.log(sYear.value)
     //console.log(data)
-    setPlayerDropdown(data)
-  }
+    setPlayerDropdown(data);
+  };
 
   //Get all stats of a players from all tournaments found in database
-  const getPlayerStats = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/stat/${sPlayer.value}`)
-    const data = res.data
+  const getPlayerStats = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/stat/${sPlayer.value}`
+    );
+    const data = res.data;
     //console.log(data)
-    setStats(data)
+    setStats(data);
     setIsLoading(false);
-  }
+  };
 
   //Get distinct years a player has played
-  const getDistinctPlayedYears = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/distinctyearsplayed/${sPlayer.value}`)
-    const data = res.data
+  const getDistinctPlayedYears = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/distinctyearsplayed/${sPlayer.value}`
+    );
+    const data = res.data;
     //console.log(data)
     //console.log(data.reverse())
-    setDistinctYears(data)
-  }
+    setDistinctYears(data);
+  };
 
   //Get the average Rank This Week for a player per year
   //Length of this array should be same as player's distinct years played
-  const getRankTWAvg = async() => {
-    let avgList = []
+  const getRankTWAvg = async () => {
+    let avgList = [];
     var i, x;
-    for(i = 0; i < distinctPlayerYears.length; i++) {
-      const res = await axios.get(`https://golfda.herokuapp.com/api/golf/ranktw/${distinctPlayerYears[i]}/${sPlayer.value}`)
-      const data = res.data
-      let sum = 0
-      for(x = 0; x < data.length; x++) {
-        sum += data[x]
+    for (i = 0; i < distinctPlayerYears.length; i++) {
+      const res = await axios.get(
+        `https://golf-da-api.onrender.com/api/golf/ranktw/${distinctPlayerYears[i]}/${sPlayer.value}`
+      );
+      const data = res.data;
+      let sum = 0;
+      for (x = 0; x < data.length; x++) {
+        sum += data[x];
       }
-      avgList.push(sum/data.length)
+      avgList.push(sum / data.length);
     }
-    setRankTWAvg(avgList)
+    setRankTWAvg(avgList);
     //console.log(distinctPlayerYears)
-  }
+  };
 
   //Get the tournament a specific player has played in a specfic year
-  const getPlayerPlayedTourneysInYear = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/playedtournsyear/${sYear.value}/${sPlayer.value}`)
-    const data = res.data
-    setPlayerPlayedTourneysInYear(data)
-  }
+  const getPlayerPlayedTourneysInYear = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/playedtournsyear/${sYear.value}/${sPlayer.value}`
+    );
+    const data = res.data;
+    setPlayerPlayedTourneysInYear(data);
+  };
 
   //Get the Rank of specific player in each tournament in a specific year
-  const getPlayerRanksInYear = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/playerranksinyear/${sYear.value}/${sPlayer.value}`)
-    const data = res.data
-    let fixedData = []
-    var x
-    for(x = 0; x < data.length; x++){
-      fixedData.push(data[x]["Rank This Week"])
+  const getPlayerRanksInYear = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/playerranksinyear/${sYear.value}/${sPlayer.value}`
+    );
+    const data = res.data;
+    let fixedData = [];
+    var x;
+    for (x = 0; x < data.length; x++) {
+      fixedData.push(data[x]["Rank This Week"]);
     }
-    setRanksInYear(fixedData)
-  }
+    setRanksInYear(fixedData);
+  };
 
-   //Get the Average Strokes of specific player in each tournament in a specific year
-  const getPlayerScoresInYear = async() => {
-    const res = await axios.get(`https://golfda.herokuapp.com/api/golf/playerscoresinyear/${sYear.value}/${sPlayer.value}`)
-    const data = res.data
-    let fixedData = []
-    var x
-    for(x = 0; x < data.length; x++){
-      fixedData.push(data[x].Average)
+  //Get the Average Strokes of specific player in each tournament in a specific year
+  const getPlayerScoresInYear = async () => {
+    const res = await axios.get(
+      `https://golf-da-api.onrender.com/api/golf/playerscoresinyear/${sYear.value}/${sPlayer.value}`
+    );
+    const data = res.data;
+    let fixedData = [];
+    var x;
+    for (x = 0; x < data.length; x++) {
+      fixedData.push(data[x].Average);
     }
-    setScoresInYear(fixedData)
-  }
+    setScoresInYear(fixedData);
+  };
 
   //// OPTIONS MENU ONSUBMIT FUNCTIONS///////
   //When a year is selected
-  function newYearSetter(option){   
-    setSelectYear(option)
+  function newYearSetter(option) {
+    setSelectYear(option);
   }
 
   //When a tournament is selected
-  const newTourneySetter = async(option) =>{   
-    setSelectTourney(option)
-  }
+  const newTourneySetter = async (option) => {
+    setSelectTourney(option);
+  };
 
   //When a player is selected
-  const newPlayerDropSetter = async(option) =>{
-    if(sPlayer){setIsLoading(true)}   
-    setSelectPlayer(option)
-  }
-
-
-
+  const newPlayerDropSetter = async (option) => {
+    if (sPlayer) {
+      setIsLoading(true);
+    }
+    setSelectPlayer(option);
+  };
 
   //////Misc. functions
 
@@ -230,100 +247,112 @@ function App() {
         data: rankTWAvg,
         fill: false,
         lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
         borderDash: [],
         borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
         pointBorderWidth: 1,
         pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
         pointHoverBorderWidth: 2,
         pointRadius: 1,
         pointHitRadius: 10,
-      }
-    ]
-  }
+      },
+    ],
+  };
 
   //Scores in everytournament in a year and the tournament they played in
   const rankScoreLine = {
     labels: playerPlayedTourneysInYear,
     datasets: [
       {
-        type: 'line',
+        type: "line",
         label: `${sPlayer.value}'s Scores in ${sYear.value}`,
-        borderColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: "rgba(255, 99, 132, 0.2)",
         borderWidth: 2,
         fill: false,
         data: scoresInYear,
-        yAxisID: 'y-axis-2',
+        yAxisID: "y-axis-2",
       },
       {
-        type: 'line',
+        type: "line",
         label: `${sPlayer.value}'s Ranks in ${sYear.value}`,
         data: ranksInYear,
-        borderColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: "rgba(54, 162, 235, 0.2)",
         borderWidth: 2,
         fill: false,
-        yAxisID: 'y-axis-1',
+        yAxisID: "y-axis-1",
       },
     ],
-  }
+  };
 
   const scoreRankOptions = {
     scales: {
       yAxes: [
         {
-          type: 'linear',
+          type: "linear",
           display: true,
-          position: 'left',
-          id: 'y-axis-1',
+          position: "left",
+          id: "y-axis-1",
         },
         {
-          type: 'linear',
+          type: "linear",
           display: true,
-          position: 'right',
-          id: 'y-axis-2',
+          position: "right",
+          id: "y-axis-2",
           gridLines: {
             drawOnArea: false,
           },
         },
       ],
     },
-  }
-
+  };
 
   return (
     <div className="App">
-      <div className="DataInput">        
+      <div className="DataInput">
         <h1>PGA Tour Data Visualization</h1>
-        <Dropdown classname = "myYearSelect" options={years} value={sYear} onChange={newYearSetter} placeholder="Select a year" />
-        <Dropdown options={tourneys} value={sTourney} onChange={newTourneySetter} placeholder="Select a tournament" />
-        <Dropdown options={playerTourns} value={sPlayer} onChange={newPlayerDropSetter} placeholder="Select a player" />
-
+        <Dropdown
+          classname="myYearSelect"
+          options={years}
+          value={sYear}
+          onChange={newYearSetter}
+          placeholder="Select a year"
+        />
+        <Dropdown
+          options={tourneys}
+          value={sTourney}
+          onChange={newTourneySetter}
+          placeholder="Select a tournament"
+        />
+        <Dropdown
+          options={playerTourns}
+          value={sPlayer}
+          onChange={newPlayerDropSetter}
+          placeholder="Select a player"
+        />
       </div>
-      <div className='loader'>
-        <PropagateLoader color={"#FFFFFF"} loading={isLoading} size={30}/>
+      <div className="loader">
+        <PropagateLoader color={"#FFFFFF"} loading={isLoading} size={30} />
       </div>
       <div className="DataOutput container-fluid">
         <div className="row">
-
-          <div className = "col-xs-12 col-md-6 col-lg-6">
+          <div className="col-xs-12 col-md-6 col-lg-6">
             <div className="BackgroundWhite">
-              <Line  data={playerAvgRankLineChart}/>
+              <Line data={playerAvgRankLineChart} />
             </div>
           </div>
 
-          <div className = "col-xs-12 col-md-6 col-lg-6">
+          <div className="col-xs-12 col-md-6 col-lg-6">
             <div className="BackgroundWhite">
-              <Line data={rankScoreLine} options={scoreRankOptions}/> 
+              <Line data={rankScoreLine} options={scoreRankOptions} />
             </div>
           </div>
-
         </div>
       </div>
     </div>
